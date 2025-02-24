d7fbfef19b6f54277ac7be560a076b5a6db103a3
Nazar Makar <arukadooooo@gmail.com>
2025-02-24 12:33:31 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,49 +1,22 @@
-def format_linter_error(error: dict) -> dict:
 
+def format_linter_error(error: dict) -> dict:
     return {
-        "line" if key == "line_number" else
-        "column" if key == "column_number" else
-        "message" if key == "text" else
-        "name" if key == "code" else key:value
-        for key, value in error.items()
-        if key in ["column_number", "line_number", "text", "code",]
-    } | {"source": "flake8"}
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [
-            {
-                "line" if key == "line_number" else
-                "column" if key == "column_number" else
-                "message" if key == "text" else
-                "name" if key == "code" else key: value
-                for key, value in error.items()
-                if key in ["column_number", "line_number", "text", "code"]
-            } | {"source": "flake8"}
-            for error in errors
-        ],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed"
+        "status": "failed" if errors else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [
-        {
-            "errors": [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                }
-                for error in errors
-            ],
-            "path": file_path,
-            "status": "failed" if errors else "passed"
-        }
-        for file_path, errors in linter_report.items()
-    ]
+    return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
